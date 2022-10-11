2fb8e67c6874003b42263ff5f22c48e13a801b91
majidali129 <itsmajidali129@gmail.com>
2022-10-11 11:02:13 -0700

    functionality added

--- JS/script.js
+++ JS/script.js
@@ -7,34 +7,16 @@ let amount_name = document.querySelector(".amount-name");
 // console.log(amount_name)
 
 
-
-    function addIncome(){
-        let name =  document.getElementById("name").value;
-       let newAmount =  Number.parseInt(document.getElementById("amount").value);
-       if(name != "" && newAmount != ""){
-        let balance = document.querySelector(".balance");
-        let income = document.querySelector(".income");
-        balance.innerHTML = Number.parseInt( balance.innerHTML) + newAmount;
-        income.innerHTML = Number.parseInt(income.innerHTML) + newAmount;
- 
-        
-    }
-    validateFields();
-   }
-
-   function addExpense(){
-    let name =  document.getElementById("name").value;
-    let newAmount =  Number.parseInt(document.getElementById("amount").value);
-    if(name != "" && newAmount != ""){
-    let balance = document.querySelector(".balance");
-    let income = document.querySelector(".income");
-    let expense = document.querySelector(".expense");
-    expense.innerHTML = Number.parseInt(expense.innerHTML) + newAmount;
-    income.innerHTML = Number.parseInt(income.innerHTML) - newAmount;
-    balance.innerHTML = Number.parseInt(balance.innerHTML) - newAmount;
-    }
-    validateFields()
-   }
+// accept_and_store_values_to_transaction_list
+// let data = {};
+// function setProperties() {
+//     data['balance'] = document.querySelector(".balance").innerHTML;
+//     data['income'] = document.querySelector(".income").innerHTML;
+//     data['expense'] = document.querySelector(".expense").innerHTML;
+//     data['name'] = document.getElementById("name").value;
+//     data['newAmount'] = parseInt(document.getElementById("amount").value);
+//     upDateValues();
+// };
 
 function validateFields() {
     let name = document.getElementById("name").value;
@@ -43,42 +25,99 @@ function validateFields() {
         alert("Fields can't be empty.")
     }
     else {
-        clearFields(document.getElementById("name"), document.getElementById("amount"))
-        // console.log(name)
-        // console.log(amount)
-
+        setProperties()
     }
 }
-// clear fields 
-function clearFields(field1, field2) {
-    setProperties()
-    field1.value = ""
-    field2.value = "";
+
+
+let data = [];
+function setProperties() {
+    let name = document.getElementById("name").value;
+    let userNewAmount = parseInt(document.getElementById("amount").value);
+    data.push({
+        balance: document.querySelector(".balance").innerHTML,
+        income: document.querySelector(".income").innerHTML,
+        expense: document.querySelector(".expense").innerHTML,
+        name: name,
+        newAmount: userNewAmount
+
+    })
+    localStorage.setItem("userData",JSON.stringify(data));
+    upDateValues();
+    // console.log(data)
+    // upDateValues();
+
 }
 
-// accept_and_store_values_to_transaction_list
-let data = {};
-function setProperties() {
-    data['balance'] = document.querySelector(".balance").innerHTML;
-    data['income'] = document.querySelector(".income").innerHTML;
-    data['expense'] = document.querySelector(".expense").innerHTML;
-    data['name'] =  document.getElementById("name").value;
-    data['newAmount'] =  parseInt(document.getElementById("amount").value);
-    upDateValues();
-};
-
-function upDateValues(){
+function upDateValues() {
     let transaction_container = document.querySelector('.transaction-container');
-    transaction_container.innerHTML += `
-    <div class="transaction">
-    <span class="amount-name referance">${data.name}</span>
-    <span class="amount set-fz">$${data.newAmount}</span>
-    <span class="remove-amount remove-btn">X</span>
-</div>
-    `;
-    
+    transaction_container.innerHTML = "";
+    data.map((element,index)=>{
+        return transaction_container.innerHTML += `
+        <div class="transaction" id=${index}>
+        <div>
+        <span class="amount-name referance">${element.name}</span>
+        <span class="amount set-fz">$${element.newAmount}</span>
+        <span class="remove-amount remove-btn" onclick = "removeTransaction(this)">X</span>
+        </div>
+    </div>
+        `;
+    })
+    // transaction_container.style.height = "113px";
+    transaction_container.style.overflow = "scroll"
+    clearFields(document.getElementById("name"), document.getElementById("amount"))
+
+}
+
+function addIncome() {
+    let name = document.getElementById("name").value;
+    let newAmount = Number.parseInt(document.getElementById("amount").value);
+    if (name != "" && newAmount != "") {
+        let balance = document.querySelector(".balance");
+        let income = document.querySelector(".income");
+        balance.innerHTML = Number.parseInt(balance.innerHTML) + newAmount;
+        income.innerHTML = Number.parseInt(income.innerHTML) + newAmount;
+
+        }
+    validateFields()
+}
+
+function addExpense() {
+    let name = document.getElementById("name").value;
+    let newAmount = Number.parseInt(document.getElementById("amount").value);
+    if (name != "" && newAmount != "") {
+        let balance = document.querySelector(".balance");
+        let income = document.querySelector(".income");
+        let expense = document.querySelector(".expense");
+        expense.innerHTML = Number.parseInt(expense.innerHTML) + newAmount;
+        income.innerHTML = Number.parseInt(income.innerHTML) - newAmount;
+        balance.innerHTML = Number.parseInt(balance.innerHTML) - newAmount;
+    }
+    validateFields()
+}
+
+
+(
+    ()=>{
+        data = JSON.parse(localStorage.getItem("userData"));
+        upDateValues()
+        
+        }
+)()
+// clear fields 
+function clearFields(field1, field2) {
+    // setProperties()
+    field1.value = ""
+    field2.value = "";
 }
 
 
 // add-total_income and expense
 
+
+function removeTransaction(e){
+    e.parentElement.parentElement.remove();
+    data.splice(e.parentElement.parentElement.index,1);
+    localStorage.setItem("userData",JSON.stringify(data))
+    
+}
